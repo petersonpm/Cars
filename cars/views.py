cf7c37ba9774b0eb1c56a725ebc064eacef59539
unknown <teste@teste.com>
2024-11-21 15:44:22 -0300

    filter test3

--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,6 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.filter(model__contains='che')
+    cars = Car.objects.filter(model__contains='chevro')
     
     return render(request, 'cars.html', {'cars': cars})
