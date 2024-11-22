8bb2a24203e1790dc9f1ba1d9753c613980d81b0
unknown <teste@teste.com>
2024-11-21 22:05:06 -0300

    get configuration

--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,11 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.filter(model__contains='chevro')
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+        #cars = Car.objects.filter(model__contains='chevro')
     
     return render(request, 'cars.html', {'cars': cars})
